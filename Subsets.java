270ff6bda8f533d3cd315fed0df193c0e5c7839c
Sanjogita Mishra <57105536+SanjogitaMishra@users.noreply.github.com>
2020-07-11 20:16:11 +0530

    Create Subsets.java

---
+++ Subsets.java
@@ -0,0 +1,48 @@
+/*
+QUESTION:
+Given a set of distinct integers, nums, return all possible subsets (the power set).
+
+Note: The solution set must not contain duplicate subsets.
+
+Example:
+
+Input: nums = [1,2,3]
+Output:
+[
+  [3],
+  [1],
+  [2],
+  [1,2,3],
+  [1,3],
+  [2,3],
+  [1,2],
+  []
+]
+
+*/
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> results = new ArrayList<>();
+        
+        if (nums == null || nums.length == 0) {
+            return results;
+        }
+        
+        Arrays.sort(nums);
+        
+        List<Integer> subset = new ArrayList<>();
+        tofindallsubsets(nums, results, subset, 0);                
+        
+        return results;
+    }
+    
+    private void tofindallsubsets(int[] nums, List<List<Integer>> results, List<Integer> subset, int startIndex) {
+        results.add(new ArrayList<>(subset));
+        
+        for (int i = startIndex; i < nums.length; i++) {
+            subset.add(nums[i]);
+            tofindallsubsets(nums, results, subset, i + 1);
+            subset.remove(subset.size() - 1);            
+        }        
+    }
+}
